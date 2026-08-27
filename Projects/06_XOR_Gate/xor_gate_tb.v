b53c66a9e15518bd3fdcc65e26d542f76e5c1e42
Aary Potdar <aarypotdar01@gmail.com>
2026-08-27 14:02:09 +0530

    Add files via upload

---
+++ Projects/06_XOR_Gate/xor_gate_tb.v
@@ -0,0 +1,54 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/27/2026 01:56:04 PM
+// Design Name: 
+// Module Name: xor_gate_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module xor_gate_tb;
+    reg A;
+    reg B;
+    wire Y;
+    
+    xor_gate uut(
+    .A(A),
+    .B(B),
+    .Y(Y)
+    );
+    initial begin
+    A = 0;
+    B = 0;
+    #10;
+    
+    A = 1;
+    B = 0;
+    #10;
+    
+    A = 0;
+    B = 1;
+    #10;
+    
+    A = 1;
+    B = 1;
+    #10;
+    
+    $finish;
+    
+    end
+    
+endmodule
